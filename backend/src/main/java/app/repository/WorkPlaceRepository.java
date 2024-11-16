19d94bb3bcdf118898d4c519fd914d92c625da68
lukesh94 <quiroga9409@gmail.com>
2024-11-16 18:11:45 -0300

    Added a method to search if a workPlace exists by name

--- backend/src/main/java/app/repository/WorkPlaceRepository.java
+++ backend/src/main/java/app/repository/WorkPlaceRepository.java
@@ -4,6 +4,10 @@ import app.domain.WorkPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WorkPlaceRepository extends JpaRepository<WorkPlace, Long> {
+
+    public Optional<WorkPlace> findByName(String name);
 }
