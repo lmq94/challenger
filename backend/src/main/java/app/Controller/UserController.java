a29069e379a4c580ae31ba95256024283f2fa477
lukesh94 <quiroga9409@gmail.com>
2024-11-16 23:45:39 -0300

    added method of login

--- backend/src/main/java/app/Controller/UserController.java
+++ backend/src/main/java/app/Controller/UserController.java
@@ -1,12 +1,15 @@
 package app.Controller;
 
 import app.dto.UserDTO;
+import app.service.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import app.service.UserService;
+
+import javax.naming.AuthenticationException;
 
 @RestController
 @RequestMapping("/users")
@@ -15,15 +18,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthService authService;
+
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(this.userService.getUserById(id));
     }
 
-    @PostMapping("")
+    @PostMapping("/register")
     public ResponseEntity<?> createUser(@RequestBody @Valid UserDTO user) {
         this.userService.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body("se ha creado con exito el usuario");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserDTO user) throws AuthenticationException {
+
+            String token = authService.authUser(user.getEmail(), user.getPassword());
+
+            return ResponseEntity.ok(token);
+
     }
 
     @PatchMapping("/{id}")
