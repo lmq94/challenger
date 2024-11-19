a389b98e9430a212923cfe2a9b76ef264fcc39b8
Lucas Quiroga <quiroga9409@gmail.com>
2024-11-19 10:42:16 -0300

    addes new properties

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashBoardComponent } from "./dash-board/dash-board.component";
+import { TableComponent } from './table/table.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TableComponent,  HttpClientModule, DashBoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
