d03e138a5e48fc7dad34fa97934baffaf86d7873
Lucas Quiroga <quiroga9409@gmail.com>
2024-11-19 12:17:15 -0300

    Eliminated depreciated import

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashBoardComponent } from "./dash-board/dash-board.component";
 import { TableComponent } from './table/table.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TableComponent,  HttpClientModule, DashBoardComponent],
+  imports: [RouterOutlet, TableComponent, DashBoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
