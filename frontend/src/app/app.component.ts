0cee7ea17eaa2ce0aa464680f205051125e77890
Lucas Quiroga <quiroga9409@gmail.com>
2024-11-19 16:41:36 -0300

    eliminated unused imports

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashBoardComponent } from "./dash-board/dash-board.component";
-import { TableComponent } from './table/table.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TableComponent, DashBoardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
