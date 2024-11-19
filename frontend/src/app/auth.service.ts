385781f89fed06cf3f53eae39cd1ccdbf3a14092
lukesh94 <quiroga9409@gmail.com>
2024-11-19 20:28:30 -0300

    implemented form login

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/users/login';
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<string> {
+    const loginData = { email, password };
+    return this.http.post('http://localhost:8080/users/login', { email, password }, { responseType: 'text' });
+
+  }
 }
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  async onLogin() {
+    console.log(this.email);
+    console.log(this.password);
+
+    this.authService.login(this.email, this.password).subscribe({
+      next: async (response: string) => {  // 'response' será el token como string
+        console.log('Login exitoso. Token recibido:', response);
+        localStorage.setItem('token', response);  // Guarda el token en localStorage
+
+        try {
+          await this.router.navigate(['/dashboard']);
+          console.log('Navegación al dashboard exitosa.');
+        } catch (error) {
+          console.error('Error al navegar al dashboard:', error);
+        }
+      },
+      error: (error) => {
+        console.error('Error en el login:', error);
+        this.errorMessage = "Contraseña o usuario incorrectos.";
+      }
+
+    });
+  }
 
 }
