9842e29c738e2f7e07fbb19201d31768d661af37
lukesh94 <quiroga9409@gmail.com>
2024-11-21 01:25:40 -0300

    Added delete and edit option

--- frontend/src/app/table/table.component.ts
+++ frontend/src/app/table/table.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { WorkPlace } from '../interfaces/work-place';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {WorkPlaceService} from '../services/work-place.service';
 import {MatDialog} from '@angular/material/dialog';
 import {InsertWorkPlaceComponent} from '../insert-work-place/insert-work-place.component';
+import {EditWorkPlaceComponent} from '../edit-work-place/edit-work-place.component';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
 export class TableComponent implements OnInit  {
 
   workPlaces: WorkPlace[] = [];
+  visibleMenus: { [key: number]: boolean } = {};
 
   constructor(
     private plantService: WorkPlaceService,
@@ -39,7 +41,7 @@ export class TableComponent implements OnInit  {
 
   openInsertPlantModal() {
     const dialogRef = this.dialog.open(InsertWorkPlaceComponent, {
-      width: '500px',
+      width: '800px',
       disableClose: true
     });
 
@@ -47,5 +49,46 @@ export class TableComponent implements OnInit  {
       this.fetchPlants();
     });
   }
+
+  openEditWorkPlaceModal(workplace: WorkPlace) {
+    const dialogRef = this.dialog.open(EditWorkPlaceComponent, {
+      width: '800px',
+      disableClose: true,
+      data: workplace
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.fetchPlants();
+    });
+  }
+
+
+  toggleMenu(index: number): void {
+    this.visibleMenus = {};
+    this.visibleMenus[index] = !this.visibleMenus[index];
+  }
+
+  closeMenu(): void {
+    this.visibleMenus = {};
+  }
+
+
+  editWorkPlace(workplace: WorkPlace): void {
+    this.openEditWorkPlaceModal(workplace)
+  }
+
+
+  deleteWorkPlace(workplace: any): void {
+    this. plantService.deleteWorkPlace(workplace.id).subscribe({
+      next: () => {
+        this.fetchPlants();
+      },
+      error: (error) => {
+        console.error('Error al eliminar la planta:', error);
+      }
+    });
+  }
+
+
 
 }
