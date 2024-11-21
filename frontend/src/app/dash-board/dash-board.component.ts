f370647344dc28586397aa8d03cc7fa3e46845b5
lukesh94 <quiroga9409@gmail.com>
2024-11-21 01:22:49 -0300

    Refactor visual aspect

--- frontend/src/app/dash-board/dash-board.component.ts
+++ frontend/src/app/dash-board/dash-board.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { TableComponent } from "../table/table.component";
 import { CardData } from '../interfaces/card-data';
 import { CardService } from '../services/card-service';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-dash-board',
   standalone: true,
-  imports: [TableComponent, CommonModule, SidebarComponent],
+  imports: [TableComponent, CommonModule, SidebarComponent, NgOptimizedImage],
   templateUrl: './dash-board.component.html',
   styleUrl: './dash-board.component.css'
 })
