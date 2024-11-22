9adc228638ad54be501d49535426abb9d18e6529
lukesh94 <quiroga9409@gmail.com>
2024-11-22 01:46:03 -0300

    update Cors policies

--- backend/src/main/java/app/config/WebConfig.java
+++ backend/src/main/java/app/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH");
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH","OPTIONS");
     }
 
 }
